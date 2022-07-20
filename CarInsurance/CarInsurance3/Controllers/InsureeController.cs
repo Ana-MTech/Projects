c9fb6209fb43baa0267f90ad183c86d9148bed45
Ana-MTech <75578193+Ana-MTech@users.noreply.github.com>
2022-07-20 17:52:43 +0100

    Update InsureeController.cs

--- CarInsurance/CarInsurance3/Controllers/InsureeController.cs
+++ CarInsurance/CarInsurance3/Controllers/InsureeController.cs
@@ -57,7 +57,6 @@ namespace CarInsurance3.Controllers
         {
             if (ModelState.IsValid)
             {
-                insuree.Quote = insQuote(insuree);
                 db.Insurees.Add(insuree);
                 db.SaveChanges();
                 return RedirectToAction("Admin");
@@ -90,7 +89,6 @@ namespace CarInsurance3.Controllers
         {
             if (ModelState.IsValid)
             {
-                insuree.Quote = insQuote(insuree);
                 db.Entry(insuree).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Admin");
